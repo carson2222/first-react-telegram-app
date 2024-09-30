0069d31c87cb055270fed0e74234b328eb57875f
carson2222 <olcraftyt@gmail.com>
2024-09-30 13:28:03 +0200

    init

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import WebApp from "@twa-dev/sdk";
+
+WebApp.ready();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
